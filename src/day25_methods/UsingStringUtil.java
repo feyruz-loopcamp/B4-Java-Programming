40c216bee2275e0ec1749f78800ebd71996deb1d
feyruz-loopcamp <feyruz@loopcamp.io>
2024-12-11 22:07:37 -0500

    Reusable classes with custom methods

---
+++ src/day25_methods/UsingStringUtil.java
@@ -0,0 +1,24 @@
+package day25_methods;
+
+import my_utilities.StringUtil;
+import java.util.Arrays;
+
+public class UsingStringUtil {
+    public static void main(String[] args) {
+
+        //Arrays.sort();
+        System.out.println(StringUtil.fixFormat("jamES"));
+
+        System.out.println(StringUtil.frequencyOfLetters("loopcamp", 'o'));
+
+
+        System.out.println(StringUtil.reverse("loopcamp"));
+
+        String str = StringUtil.duplicateCharacters("loopcamp");
+        System.out.println(str);
+
+
+        System.out.println( StringUtil.duplicateCharacters("apple") );
+
+    }
+}
